ad5072faf129967306c6ab45f2d5451060507cf2
Eloisa Amorin <eloisa.amorin@keyrus.com.br>
2022-06-26 14:58:54 -0300

    Finidh login and registration page

--- src/app/views/pages/registration-page/registration-page.component.ts
+++ src/app/views/pages/registration-page/registration-page.component.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationPageComponent implements OnInit {
 
+  public registration: Array<{ textLink: string, href: string }> = [
+    { textLink: 'Finalizar Cadastro', href: '/' }
+  ];
+
+  public userRegistration: Array<{ type: string, name: string, placeholder: string, textLabel: string }> = [
+    { type: 'text', name: 'registration-page__fullname', placeholder: 'Nome Completo', textLabel: 'Nome Completo' },
+    { type: 'date', name: 'registration-page__birthdate', placeholder: 'Data de Nascimento', textLabel: 'Data de Nascimento' },
+    { type: 'text', name: 'registration-page__gender', placeholder: 'Gênero', textLabel: 'Gênero' },
+    { type: 'text', name: 'registration-page__cpf', placeholder: 'CPF', textLabel: 'CPF' },
+    { type: 'text', name: 'registration-page__phone', placeholder: '(00) 00000-0000', textLabel: 'Número de Telefone' },
+    { type: 'password', name: 'registration-page__password', placeholder: 'Senha', textLabel: 'Senha' },
+    { type: 'password', name: 'registration-page__password-confirm', placeholder: 'Senha', textLabel: 'Senha' }
+  ];
+
+  public addressRegistration: Array<{ type: string, name: string, placeholder: string, textLabel: string }> = [
+    { type: 'text', name: 'registration-page__zipcode', placeholder: '00000-000', textLabel: 'CEP' },
+    { type: 'text', name: 'registration-page__address', placeholder: 'Endereço', textLabel: 'Endereço' },
+    { type: 'number', name: 'registration-page__number', placeholder: 'Número', textLabel: 'Número' },
+    { type: 'text', name: 'registration-page__neighborhood', placeholder: 'Bairro', textLabel: 'Bairro' },
+    { type: 'text', name: 'registration-page__complement', placeholder: 'Complemento', textLabel: 'Complemento' },
+    { type: 'text', name: 'registration-page__city', placeholder: 'Cidade', textLabel: 'Cidade' },
+    { type: 'text', name: 'registration-page__state', placeholder: 'Estado', textLabel: 'Estado' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
